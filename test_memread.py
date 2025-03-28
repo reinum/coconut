35fd150216f2be5890bde64dd5234fe5e98c2157
reinum <me@vertexx.top>
2025-03-28 23:20:58 +0100

    kaboom

--- test_memread.py
+++ test_memread.py
@@ -17,11 +17,28 @@ def tap(key, holdtime):
     time.sleep(holdtime / 1000)  # Convert milliseconds to seconds
     interception.key_up(key)
 
-async def relax(hitObjs, firstObject, lastObject, K1, K2):
+async def relax(hitObjs, firstObject, lastObject, K1, K2, mod=None):
     currentTime = 0
     processedObjects = set()  # Keep track of processed hit objects
     keyToTapWith = K1
     realIndex = 0
+
+    # Adjust hit object timings based on the mod
+    if "DT" in mod or "NC" in mod:  # Double Time
+        speedMultiplier = 1 / 1.5
+    elif mod == "HT":  # Half Time
+        speedMultiplier = 1 / 0.75
+    else:  # No mod
+        speedMultiplier = 1.0
+
+    # Apply the speed multiplier to all hit objects
+    for obj in hitObjs:
+        obj[2] = int(obj[2] * speedMultiplier)  # Adjust objTime
+        obj[4] = int(obj[4] * speedMultiplier)  # Adjust holdTime
+
+    # Adjust first and last object times
+    firstObject = int(firstObject * speedMultiplier)
+    lastObject = int(lastObject * speedMultiplier)
 
     # Wait until the game time is synchronized
     while currentTime < firstObject - 100:  # Wait until we're close to the first object
@@ -34,7 +51,7 @@ async def relax(hitObjs, firstObject, lastObject, K1, K2):
         for index, obj in enumerate(hitObjs):  # Use enumerate to get the index
             x, y, objTime, objId, holdTime = obj
 
-            if index > realIndex+1 and realIndex != 10:
+            if index > realIndex + 1:
                 continue
 
             # Skip already processed objects
@@ -47,8 +64,7 @@ async def relax(hitObjs, firstObject, lastObject, K1, K2):
                 threading.Thread(target=tap, args=(keyToTapWith, holdTime)).start()
                 processedObjects.add(objTime)  # Mark this object as processed
                 keyToTapWith = K1 if keyToTapWith == K2 else K2
-                if realIndex <= 10:
-                    realIndex += 1
+                realIndex += 1
                 break
 
 async def waitForMap():
@@ -71,9 +87,12 @@ async def waitForMap():
     K2 = osuKeys.k2
     hitObjects = hitobjs.findHitObject(beatmap, beatmapTime.lastObject)
 
+    mods = await conn.Connection.getPlay()
+    currentMod = mods.Mods.name
+
     await asyncio.sleep(2)  # Give some time for the game to load the map
 
-    await relax(hitObjects, beatmapTime.firstObject, beatmapTime.lastObject, K1, K2)
+    await relax(hitObjects, beatmapTime.firstObject, beatmapTime.lastObject, K1, K2, currentMod)
 
 
 asyncio.run(waitForMap())
