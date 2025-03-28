ccd4b313c056c6bc620ff2c79f4e2b142a7a2e8a
reinum <me@vertexx.top>
2025-03-28 17:28:49 +0100

    určite dobrá commit správa

--- test_memread.py
+++ test_memread.py
@@ -1,18 +1,21 @@
 from tosu import tosu_connection as tosu
 from tosu import tosu_classes
 from parsers import hitobjs
+import asyncio
 
 conn = tosu.Tosu("https://localhost:20450/websocket/v2")
-conn.connect()
 
 async def relax(hit_objs, totalMapLength):
     print("wowowowo")
     
 
 async def wait_for_map():
+    await conn.connectAll()
     while True:
-        state = conn.Connection.getState()
+        state = await conn.Connection.getState()
         if state == tosu_classes.OsuState.Game:
-            map = conn.Connection.getPaths()
+            map = await conn.Connection.getPaths()
             hit_objs = hitobjs.find_hitobject(map)
             await relax(hit_objs)
+
+asyncio.run(wait_for_map())
