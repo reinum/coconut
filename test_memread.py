9378d568f1b37dfc108e6c2034b8d707f808938b
reinum <me@vertexx.top>
2025-03-28 18:50:33 +0100

    devujeme relax

--- test_memread.py
+++ test_memread.py
@@ -2,19 +2,27 @@ from tosu import tosu_connection as tosu
 from tosu import tosu_classes
 from parsers import hitobjs
 
+import interception
 import asyncio
 import os
 
+interception.auto_capture_devices()
+
 conn = tosu.Tosu("ws://localhost:24050/websocket/v2")
 
-async def relax(hitObjs, lastObject):
+async def relax(hitObjs, lastObject, K1, K2):
     currentTime = 0
     while currentTime < lastObject:
         currentTime = await conn.PreciseConnection.preciseCurrentTime()
         for obj in hitObjs:
-            if obj[2] <= currentTime <= obj[2] + obj[4]:
-                print(f"Hit object at {obj[0]}, {obj[1]}")
-                break
+            x, y, objTime, objId, holdTime = obj
+            if (objTime - currentTime) < -10 and (currentTime - objTime) > 10:  # 10ms tolerance:        
+                print(f"Hit object at {x}, {y}")
+                if objId == 0:  # Circle
+                    interception.press(K1)
+                    await asyncio.sleep(holdTime / 1000)
+                    interception.release(K1)
+                    break
 
 async def waitForMap():
     await conn.connectAll()
@@ -32,8 +40,11 @@ async def waitForMap():
     # Parse needed data from the beatmap
     hitObjects = hitobjs.findHitObject(beatmap)
     beatmapTime = await conn.Connection.getBeatmapTime()
+    osuKeys = (await conn.Connection.getKeybinds()).Osu
+    K1 = osuKeys.k1
+    K2 = osuKeys.k2
 
-    await relax(hitObjects, beatmapTime.lastObject)
+    await relax(hitObjects, beatmapTime.lastObject, K1, K2)
 
 
 asyncio.run(waitForMap())
