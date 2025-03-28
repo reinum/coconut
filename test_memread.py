9bbc34e87363c51f750c6651a3a3ffb428932464
reinum <me@vertexx.top>
2025-03-28 17:28:45 +0100

    more dyg commit asi lebo hitobj parser

--- test_memread.py
+++ test_memread.py
@@ -1,42 +1,18 @@
-import asyncio
-import websockets
-import json  # Import the JSON module
+from tosu import tosu_connection as tosu
+from tosu import tosu_classes
+from parsers import hitobjs
 
-async def listen_to_websocket(map):
-    uri = "ws://localhost:24050/websocket/v2/precise"
+conn = tosu.Tosu("https://localhost:20450/websocket/v2")
+conn.connect()
+
+async def relax(hit_objs, totalMapLength):
+    print("wowowowo")
     
-    try:
-        async with websockets.connect(uri) as websocket:
-            print(f"Connected to {uri}")
-            while True:
-                message = await websocket.recv()
-                print(f"Received: {message}")
-    except Exception as e:
-        print(f"Error: {e}")
 
-async def wait_for_playing():
-    uri = "ws://localhost:24050/websocket/v2"
-    try:
-        async with websockets.connect(uri) as websocket:
-            print(f"Connected to {uri}")
-            while True:
-                message = await websocket.recv()
-                try:
-                    # Parse the JSON string into a dictionary
-                    data = json.loads(message)
-                    status = data["state"]  # Access the "state" key
-                    print(f"State: {status}")
-                    if status["number"] == 2:
-                        map = data["directPath"]["beatmapFile"]
-                        await listen_to_websocket(map)
-                except json.JSONDecodeError:
-                    print(f"Failed to decode JSON: {message}")
-                except KeyError:
-                    print(f"'state' key not found in message: {message}")
-    except Exception as e:
-        print(f"Error: {e}")
-    
-    await listen_to_websocket()
-
-if __name__ == "__main__":
-    asyncio.run(wait_for_playing())
+async def wait_for_map():
+    while True:
+        state = conn.Connection.getState()
+        if state == tosu_classes.OsuState.Game:
+            map = conn.Connection.getPaths()
+            hit_objs = hitobjs.find_hitobject(map)
+            await relax(hit_objs)
